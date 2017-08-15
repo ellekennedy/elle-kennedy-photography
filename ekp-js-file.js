0f2d96e5f4ffc8fea4af67d949eb3074a2e90a7c
ellekennedy <30519100+ellekennedy@users.noreply.github.com>
2017-08-14 22:12:13 -0700

    Update ekp-js-file.js

--- ekp-js-file.js
+++ ekp-js-file.js
@@ -1,8 +1,13 @@
-function myFunction() {
-    var x = document.getElementById("topmenu");
-    if (x.className === "menu") {
-        x.className += " responsive";
-    } else {
-        x.className = "menu";
-    }
-}
+$(document).ready(function(){
+  $(".submenu1").hide();
+  $(".submenu2").hide();
+  $(".portfolionav").click(function(){
+    $(".submenu1").toggle();
+    $(".submenu2").hide();
+  });
+  $(".blognav").click(function(){
+    $(".submenu2").toggle();
+    $(".submenu1").hide();
+  });
+
+});
